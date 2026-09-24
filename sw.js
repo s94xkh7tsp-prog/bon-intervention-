58c795dc3ac00cedd03bbd16881e5ac0deef1623
s94xkh7tsp-prog <s94xkh7tsp@privaterelay.appleid.com>
2026-09-24 02:48:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jm-express-bon-v243';
+const CACHE_NAME = 'jm-express-bon-v246';
 
 // Seuls les fichiers indispensables doivent pouvoir bloquer l'installation.
 const APP_SHELL_REQUIRED = [
